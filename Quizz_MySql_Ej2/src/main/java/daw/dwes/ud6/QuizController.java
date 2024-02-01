6931811f9a507fcf18f5afa03796962cf9bf2559
AranchaC <aranchachicharro@gmail.com>
2024-02-01 22:09:13 +0100

    casa 01feb 22.09

--- Quizz_MySql_Ej2/src/main/java/daw/dwes/ud6/QuizController.java
+++ Quizz_MySql_Ej2/src/main/java/daw/dwes/ud6/QuizController.java
@@ -1,17 +1,24 @@
 package daw.dwes.ud6;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpSession;
 
-@Controller
+@RestController
 public class QuizController {
 	
     private final ResultadoRepository resultadoRepositorio;
@@ -301,5 +308,61 @@ public class QuizController {
             return Clasificacion.HUFFLEPUFF;
         }
     }//calcularClasif
+    
+    // MÉTODOS CONSULTAS SQL //
+    
+    @GetMapping("/resultados")
+    public List<Resultado> obtenerTodosLosResultados(){
+    	return resultadoRepositorio.findAll();
+    }//ObtenerTodos
+    
+    @GetMapping("/resultados/{id}")
+    public Optional<Resultado> buscarResultadoId(@PathVariable ("id") Long id){
+    	return resultadoRepositorio.findById(id);
+    }//BuscarId
+    
+    
+    @PostMapping("/resultados")
+    public Resultado agregarResultado(@RequestBody Resultado nuevoResultado) {
+        return resultadoRepositorio.save(nuevoResultado);
+    }//agregar
+    
+    @DeleteMapping("/resultados/{id}")
+    public ResponseEntity<Object> eliminarResultado(@PathVariable ("id") Long id) {
+        Optional<Resultado> resultadoOptional = resultadoRepositorio.findById(id);
+
+        if (!resultadoOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        resultadoRepositorio.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }//eleminar
+    
+    @PutMapping("/resultados/{id}")
+    public ResponseEntity<Object> actualizarResultado(
+    		@PathVariable (name="id") Long id, 
+    		@RequestBody Resultado resultadoActualizado) {
+        Optional<Resultado> resultadoOptional = resultadoRepositorio.findById(id);
+
+        if (!resultadoOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        resultadoActualizado.setId(id); 
+        resultadoRepositorio.save(resultadoActualizado);
+        return ResponseEntity.noContent().build();
+    }//actualizar
+    
+    @GetMapping("/resultados/count")
+    public ResponseEntity<?> contarResultados() {
+        long totalResultados = resultadoRepositorio.count();
+        return ResponseEntity.ok("Número de resultados en la BBDD: " 
+        		+ totalResultados);
+    }
+
+    
+    
+
+
 }
 
--- Quizz_MySql_Ej2/src/main/resources/schema.sql
+++ Quizz_MySql_Ej2/src/main/resources/schema.sql
@@ -4,8 +4,3 @@ CREATE TABLE resultado (
     puntos INT NOT NULL,
     nombre VARCHAR(100)
 );
-
-INSERT INTO resultado (clasificacion, puntos, nombre) VALUES ('GRYFFINDOR', 25, 'Arancha');
-INSERT INTO resultado (clasificacion, puntos, nombre) VALUES ('RAVENCLAW', 18, 'Rodri');
-INSERT INTO resultado (clasificacion, puntos, nombre) VALUES ('SLYTHERIN', 14, 'Carol');
-INSERT INTO resultado (clasificacion, puntos, nombre) VALUES ('HUFFLEPUFF', 8, 'Luis');
